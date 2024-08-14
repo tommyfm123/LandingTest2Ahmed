6b48a7d7cf0a9d917454a8ae9131818fd8a13fa8
Tomás Fernández Murga <106606402+tommyfm123@users.noreply.github.com>
2024-08-14 10:32:34 -0300

    correcion api para lead, saque custom

--- facebook-capi-backend/server.js
+++ facebook-capi-backend/server.js
@@ -8,26 +8,32 @@ app.use(express.json());
 app.post('/send-event', async (req, res) => {
     const event = req.body;
 
-    const response = await fetch(`https://graph.facebook.com/v13.0/${process.env.PIXEL_ID}/events?access_token=${process.env.ACCESS_TOKEN}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            "event_name": "lead",
-            "event_time": Math.floor(Date.now() / 1000),
-            "user_data": event.user_data,
-            "custom_data": event.custom_data,
-            "event_source_url": "https://legal.miolawfirm.com",
-            "action_source": "website"
-        })
-    });
+    // Elimina `custom_data` para el evento `lead`
+    const eventPayload = {
+        "event_name": "lead",
+        "event_time": Math.floor(Date.now() / 1000),
+        "user_data": event.user_data,
+        "event_source_url": event.event_source_url,
+        "action_source": "website"
+    };
 
-    const result = await response.json();
-    res.send(result);
+    try {
+        const response = await fetch(`https://graph.facebook.com/v13.0/${process.env.PIXEL_ID}/events?access_token=${process.env.ACCESS_TOKEN}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(eventPayload)
+        });
+
+        const result = await response.json();
+        res.send(result);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(3000, () => {
     console.log('Server is running at 3000');
 });
-
